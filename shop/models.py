2aae4f0876f656f47ac25aa4e66a4d83b166385c
Shahriarhossain123 <9763691+Shahriarhossain123@users.noreply.github.com>
2021-10-07 00:40:11 +0600

    model 1

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+
 # Create your models here.
+class Products(models.Model):
+    title = models.CharField("Title", max_length=200)
+    price = models.FloatField("Price")
+    discount_price = models.FloatField("Discount Price")
+    category = models.TextField("Category")
+    description = models.TextField("Description")
+    image = models.CharField(max_length=300)
+
+    def __str__(self):
+        return self.title
